62322a6ff16478e8e6be8cafd48f29ff29a37d0f
Kasia Dav <k.dawidowicz2016@gmail.com>
2019-05-05 15:14:23 +0200

    Updates

---
+++ precache-manifest.cc64b7a631dc2535d53829e8e0443b81.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c3ba32f34dee4563dcf318751ae96d",
+    "url": "/Cafe-project/index.html"
+  },
+  {
+    "revision": "83e911d5e42ba3e39fc8",
+    "url": "/Cafe-project/static/css/main.053a887c.chunk.css"
+  },
+  {
+    "revision": "0bbb0ad7456696ded249",
+    "url": "/Cafe-project/static/js/2.603f61cc.chunk.js"
+  },
+  {
+    "revision": "83e911d5e42ba3e39fc8",
+    "url": "/Cafe-project/static/js/main.5b979453.chunk.js"
+  },
+  {
+    "revision": "f3a50e08351fd45fb764",
+    "url": "/Cafe-project/static/js/runtime~main.114dd31c.js"
+  },
+  {
+    "revision": "d970546cee775b3958a30b9afdb50bfe",
+    "url": "/Cafe-project/static/media/cake1.d970546c.jpg"
+  },
+  {
+    "revision": "cdebb431939aa6402d0d7908c4acfd9b",
+    "url": "/Cafe-project/static/media/cake2.cdebb431.jpg"
+  },
+  {
+    "revision": "7284d09a283144c0043c67696a60843b",
+    "url": "/Cafe-project/static/media/ice1.7284d09a.jpg"
+  },
+  {
+    "revision": "165f46bfb9786346215ae43f0c99ceb4",
+    "url": "/Cafe-project/static/media/ice2.165f46bf.jpg"
+  },
+  {
+    "revision": "4f903cf2928bb11a2aea89eb52e42454",
+    "url": "/Cafe-project/static/media/kawa2.4f903cf2.jpg"
+  },
+  {
+    "revision": "dc7d1c15436bed2c544679daebe8d95b",
+    "url": "/Cafe-project/static/media/kawa3.dc7d1c15.jpg"
+  },
+  {
+    "revision": "5b87640eadf8dafc90ac3c7b6055006d",
+    "url": "/Cafe-project/static/media/main2.5b87640e.jpg"
+  },
+  {
+    "revision": "beaa1f429e21f0c806ffb3c722e4a0c6",
+    "url": "/Cafe-project/static/media/outside3.beaa1f42.jpg"
+  },
+  {
+    "revision": "9184c1d0237cfca439862b6627824308",
+    "url": "/Cafe-project/static/media/table.9184c1d0.jpg"
+  },
+  {
+    "revision": "ee28482ef0a811e01080567371a52152",
+    "url": "/Cafe-project/static/media/tea.ee28482e.jpg"
+  },
+  {
+    "revision": "57add3a2e973e616efbaa859afbd6181",
+    "url": "/Cafe-project/static/media/tea2.57add3a2.jpg"
+  },
+  {
+    "revision": "3eb84906324eb1b75be66b518a99b212",
+    "url": "/Cafe-project/static/media/widok.3eb84906.jpg"
+  }
+]);
